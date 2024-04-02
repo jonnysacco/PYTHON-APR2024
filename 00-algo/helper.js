12cec4706e89d6977ef46441577a2d8abeb446b1
heidi1105 <heidi1105@gmail.com>
2024-04-02 08:49:58 -0600

    algo

---
+++ 00-algo/helper.js
@@ -0,0 +1,15 @@
+// String
+
+message = "   Hello World"
+console.log(message[3])
+
+// To return the string/character in upper case
+console.log(message.toUpperCase())
+
+// To remove space in front / at the end
+console.log(message.trim())
+
+// To seprate a sentence into different elements and store in the array
+sentence = "Morning Ninjas! Welcome to the class"
+wordList = sentence.split(" ")
+console.log(wordList)
