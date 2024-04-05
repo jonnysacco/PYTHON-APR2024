9399a989f2cd26193af487cd5bb68f69f6e15bc9
heidi1105 <heidi1105@gmail.com>
2024-04-05 15:14:37 -0600

    form

--- 03-flask/flask3-session-form/server.py
+++ 03-flask/flask3-session-form/server.py
@@ -2,23 +2,28 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = "type whatever you want as long as it is a secret for you"
 
-@app.route("/")
-def test_session_counter_page():
-    # 1. If the session is available
-    # 2. update the session (by incrementing)
-    # 3. If the session is not available  
-    # 4. set the initial session
-    pass # replace pass with your code
-
-@app.route("/clear")
-def clear_session():
-    # Clear the session and redirect
-    pass
-
 # Step 1: GET request to render a page to display the form.html (how to create a form)
+@app.route("/bugs/new")
+def new_bug_form():
+    return render_template("new_bug.html")
+
 # Step 2: Create a POST function to receive the POST form request 
-# Step 3: Process the data (store it in session) and redirect to other routes
+@app.route("/bugs/new/process", methods=["POST"]) # Route must match form action
+def process_new_bug():
+    print("In post route")
+    print(request.form)
+    print(request.form["title"])
+    # Step 3: Process the data (store it in session) and redirect to other routes
+    session["title"] = request.form["title"]
+    session["assignment"] = request.form["assignment"]
+    session["description"] = request.form["description"]
+    session["username"] = request.form["username"]
+    return redirect("/bugs/result")
+
 # Step 4: Add that redirected route & template file
+@app.route("/bugs/result")
+def bug_result_page():
+    return render_template("bug_result.html")
 
 if __name__=="__main__":   
-    app.run(debug=True) 
+    app.run(debug=True, port=5001) 
---
+++ 03-flask/flask3-session-form/temp.py
@@ -0,0 +1,12 @@
+@app.route("/")
+def test_session_counter_page():
+    # 1. If the session is available
+    # 2. update the session (by incrementing)
+    # 3. If the session is not available  
+    # 4. set the initial session
+    pass # replace pass with your code
+
+@app.route("/clear")
+def clear_session():
+    # Clear the session and redirect
+    pass
